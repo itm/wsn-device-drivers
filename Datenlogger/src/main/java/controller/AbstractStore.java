08c77a9712e2a38c5dfb22517a116cdc0374ca19
fkausche <fkausche@github.com>
2010-10-18 13:51:32 +0000

    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@139 0a1f239c-e987-4cba-9274-3ef986406e12

---
+++ Datenlogger/src/main/java/controller/AbstractStore.java
@@ -0,0 +1,59 @@
+package controller;
+
+import model.Key;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @param <K>
+ * @param <V>
+ */
+public abstract class AbstractStore<K, V extends Key> {
+
+    private final Map<K, V> store;
+
+    protected AbstractStore() {
+        this.store = new HashMap<K, V>();
+    }
+
+    /**
+     * Adds a new value in the store.
+     *
+     * @param value
+     */
+    public final void add(final V value) {
+        store.put((K) value.getKey(), value);
+    }
+
+    /**
+     * Looksup the  store and retrieves values based on the key.
+     *
+     * @param key
+     * @return
+     */
+    public final V get(final K key) {
+        return store.get(key);
+    }
+
+
+    /**
+     * Returns the number of entities stored in the hashmap.
+     *
+     * @return the size of the hashmap.
+     */
+    public final int size() {
+        return store.size();
+    }
+
+    /**
+     * @return a list of all the values in the store.
+     */
+    public final List<V> list() {
+        return new ArrayList<V>(store.values());
+    }
+
+
+}
