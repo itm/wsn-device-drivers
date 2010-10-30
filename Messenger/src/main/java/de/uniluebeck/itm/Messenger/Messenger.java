b7108b038b34ede53b592fb2f403f2ccdcdbf97c
fkausche <fkausche@github.com>
2010-10-30 13:18:46 +0000

    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@157 0a1f239c-e987-4cba-9274-3ef986406e12

--- Messenger/src/main/java/de/uniluebeck/itm/Messenger/Messenger.java
+++ Messenger/src/main/java/de/uniluebeck/itm/Messenger/Messenger.java
@@ -1,5 +1,8 @@
 package de.uniluebeck.itm.Messenger;
 
+import de.uniluebeck.itm.devicedriver.async.AsyncCallback;
+import de.uniluebeck.itm.devicedriver.nulldevice.NullDevice;
+
 public class Messenger {
 	
 	String port;
@@ -22,6 +25,30 @@ public class Messenger {
 		System.out.println("Port: " + port);
 		System.out.println("Server: " + server);
 		System.out.println("Message: " + message);
-		System.out.println("\nDie Nachricht wurde erfolgreich gesendet.");
+		
+		NullDevice nullDevice = new NullDevice();
+		
+		AsyncCallback<Void> callback = new AsyncCallback<Void>() {
+			public void onSuccess(Void result) {
+				System.out.println("Die Nachricht wurde gesendet.");
+			}
+			public void onCancel() {
+				System.out.println("Die Operation wurde abgebrochen");
+			}
+			public void onFailure(Throwable throwable) {
+				throwable.printStackTrace();
+			}
+			public void onProgressChange(float fraction) {
+				System.out.println("Es tut sich was.");
+			}
+		};
+	
+		try {
+			nullDevice.createSendOperation().execute(callback);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.exit(0);
 	}
 }
