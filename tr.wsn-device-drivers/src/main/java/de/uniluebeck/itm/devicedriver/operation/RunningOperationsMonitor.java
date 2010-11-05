71f173301ae4efcfbcfe626202376882c1796254
mlegenhausen <mlegenhausen@github.com>
2010-11-05 13:55:32 +0000

    Comments added and some code clean ups.
    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@180 0a1f239c-e987-4cba-9274-3ef986406e12

--- tr.wsn-device-drivers/src/main/java/de/uniluebeck/itm/devicedriver/operation/RunningOperationsMonitor.java
+++ tr.wsn-device-drivers/src/main/java/de/uniluebeck/itm/devicedriver/operation/RunningOperationsMonitor.java
@@ -27,15 +27,27 @@ public class RunningOperationsMonitor {
 		operation.addListener(new OperationAdapter<T>() {
 			@Override
 			public void onStateChanged(Operation<T> operation, State oldState, State newState) {
-				synchronized (operations) {
-					if (operation.getState() == State.RUNNING) {
-						operations.add(operation);
-					} else {
-						operations.remove(operation);
-					}
-				}
+				RunningOperationsMonitor.this.onStateChanged(operation, oldState, newState);
 			}
 		});
+	}
+	
+	/**
+	 * When an operation is in running state it is added to the internal list.
+	 * 
+	 * @param <T> The return type of the operation.
+	 * @param operation The operation that changed the state.
+	 * @param oldState The state before.
+	 * @param newState The new state.
+	 */
+	private <T> void onStateChanged(Operation<T> operation, State oldState, State newState) {
+		synchronized (operations) {
+			if (newState == State.RUNNING) {
+				operations.add(operation);
+			} else {
+				operations.remove(operation);
+			}
+		}
 	}
 	
 	/**
