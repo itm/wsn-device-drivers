d19f4851b508a3983dc4c7e1dd598ed4212e0408
fkausche <fkausche@github.com>
2010-12-05 15:34:31 +0000

    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@268 0a1f239c-e987-4cba-9274-3ef986406e12

---
+++ tr.wsn-device-apps-messenger/src/test/java/de/uniluebeck/itm/messenger/MessengerTest.java
@@ -0,0 +1,23 @@
+package de.uniluebeck.itm.Messenger;
+
+import junit.framework.TestCase;
+
+public class MessengerTest extends TestCase {
+
+	public void testSetPort(){
+		Messenger messenger = new Messenger();
+	    messenger.setPort("123");
+	    assertEquals("123", messenger.port);
+	}
+	
+	public void testSetServer(){
+		Messenger messenger = new Messenger();
+	    messenger.setServer("123");
+	    assertEquals("123", messenger.server);
+	}
+	
+	public void testSend(){
+		Messenger messenger = new Messenger();
+	    messenger.send("123");
+	}
+}
