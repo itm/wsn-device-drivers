f31b9cbf5fa9bdd4ba00a30c01d0b9a75ada2586
AMaier <AMaier@github.com>
2010-12-05 14:25:05 +0000

    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@235 0a1f239c-e987-4cba-9274-3ef986406e12

---
+++ tr.wsn-device-apps-messenger/src/main/java/de/uniluebeck/itm/Messenger/Main.java
@@ -0,0 +1,65 @@
+package de.uniluebeck.itm.Messenger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.commons.cli.*;
+
+public class Main {
+	
+	private static double version = 0.1;
+	
+	public static void main(String[] args) throws IOException {
+		// create Options object
+		Option help_option = new Option( "help", "print this message" );
+		Option version_option = new Option( "version", "print the version information" );
+		
+		Options options = new Options();
+		
+		options.addOption(help_option);
+		options.addOption(version_option);
+		
+		// add options for Messenger
+		options.addOption("port", true, "port");
+		options.addOption("server", true, "server");
+		options.addOption("message", true, "Die Nachricht, die verschickt werden soll in Hex-Code");
+		
+		// for help statement
+		HelpFormatter formatter = new HelpFormatter();
+
+		CommandLineParser parser = new GnuParser();
+		CommandLine cmd = null;
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			System.out.println("Diese Option gibt es nicht.");
+		}
+		if(cmd != null){
+			//standard-options
+			if(cmd.hasOption("help")){
+				System.out.println("Aufrufbeispiele:");
+				System.out.println("Messenger: send -message 0a 3f 41 -port 141.83.1.546:1282");
+				System.out.println("");
+				formatter.printHelp("help", options);
+			}
+			if(cmd.hasOption("version")){
+				System.out.println(version);
+			}
+			
+			//der Messenger
+			if(args[0].equals("send")) {
+				System.out.println("starte Messenger...");
+				
+				String port = cmd.getOptionValue("port");
+				String server = cmd.getOptionValue("server");
+				String message = cmd.getOptionValue("message");
+				
+				Messenger messenger = new Messenger();
+				messenger.setPort(port);
+				messenger.setServer(server);
+				messenger.send(message);							
+			}
+		}
+	}
+}
