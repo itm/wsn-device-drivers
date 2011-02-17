e24aa3db58b9219b10bc51da82de52bd0214094f
fkausche <fkausche@github.com>
2011-02-17 19:29:49 +0000

    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@536 0a1f239c-e987-4cba-9274-3ef986406e12

--- tr.wsn-device-apps-datenlogger/src/test/java/de/uniluebeck/itm/datenlogger/DatenloggerTest.java
+++ tr.wsn-device-apps-datenlogger/src/test/java/de/uniluebeck/itm/datenlogger/DatenloggerTest.java
@@ -5,50 +5,50 @@ import junit.framework.TestCase;
 public class DatenloggerTest extends TestCase {
 
 	public void testSetPort(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.setPort("123");
 	    assertEquals("123", logger.getPort());
 	}
 	
 	public void testSetServer(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.setServer("123");
 	    assertEquals("123", logger.getServer());
 	}
 	
 	public void testSetGestartet(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.setStartet(true);
 	    assertEquals(logger.isStarted(), true);
 	}
 	
 	public void testSetKlammerFilter(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.setKlammer_filter("(uint32,14,2)");
 	    assertEquals(logger.getKlammer_filter(), "(uint32,14,2)");
 	}
 	
 	public void testSetRegexFilter(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.setRegex_filter("a&b|c");
 	    assertEquals(logger.getRegex_filter(), "a&b|c");
 	}
 	
 	public void testSetLocation(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.setLocation("123");
 	    assertEquals(logger.getLocation(), "123");
 	}
 	
 	public void testStartlog(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.connect();
 		logger.startlog();
 		assertEquals(true, logger.isStarted());
 	}
 	
 	public void testStoplog(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.connect();
 		logger.startlog();
 		logger.stoplog();
@@ -56,21 +56,21 @@ public class DatenloggerTest extends TestCase {
 	}
 	
 	public void testAdd_klammer_filter(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.setKlammer_filter("(uint32,6,28)");
 		logger.add_klammer_filter("&(uint32,5,17)|(int16,0,3)");
 		assertEquals("(uint32,6,28)&(uint32,5,17)|(int16,0,3)", logger.getKlammer_filter());
 	}
 	
 	public void testAdd_regex_filter(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.setRegex_filter("(a&b)");
 		logger.add_regex_filter("|c");
 		assertEquals("(a&b)|c", logger.getRegex_filter());
 	}
 	
 	public void testWriteToXmlFile(){
-		Datenlogger logger = new Datenlogger();
+		Datalogger logger = new Datalogger();
 		logger.writeToXmlFile();
 		//TODO ...
 	}
