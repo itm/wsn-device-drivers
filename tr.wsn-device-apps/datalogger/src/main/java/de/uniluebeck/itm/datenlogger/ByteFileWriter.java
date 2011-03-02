e1db505dce9262a177994a2a3c6c60966e1bd121
fkausche <fkausche@github.com>
2011-03-02 21:46:51 +0000

    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@610 0a1f239c-e987-4cba-9274-3ef986406e12

--- tr.wsn-device-apps/datalogger/src/main/java/de/uniluebeck/itm/datenlogger/ByteFileWriter.java
+++ tr.wsn-device-apps/datalogger/src/main/java/de/uniluebeck/itm/datenlogger/ByteFileWriter.java
@@ -3,34 +3,38 @@ package de.uniluebeck.itm.datenlogger;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class ByteFileWriter extends AbstractFileWriter{
-	
+public class ByteFileWriter extends AbstractFileWriter {
+
 	FileOutputStream writer;
-	
+
 	@Override
 	public void write(byte[] content) {
 		try {
-			writer.write(content);
+			String temp = "";
+			for (byte a : content) {
+				temp += a + " ";
+			}
+			temp += "\n";
+			writer.write(temp.getBytes());
 		} catch (IOException e) {
 			System.out.println("Cannot write to file.");
 		}
-
 	}
-	
+
 	@Override
-	public void setLocation(String location){
+	public void setLocation(String location) {
 		try {
 			this.writer = new FileOutputStream(location);
 		} catch (IOException e) {
 			System.out.println("FileOutputStream could not be created.");
 		}
 	}
-	
+
 	@Override
 	public String convert(byte[] content) {
 		return "";
 	}
-	
+
 	@Override
 	public void close() throws IOException {
 		writer.close();
