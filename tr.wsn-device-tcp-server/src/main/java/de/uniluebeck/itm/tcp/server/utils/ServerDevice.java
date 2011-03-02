c4d97dc0e489b4ef29b7a40b24743f4d4bf03c84
AMaier <AMaier@github.com>
2011-03-02 19:18:06 +0000

    Thread ausgelagert
    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@604 0a1f239c-e987-4cba-9274-3ef986406e12

--- tr.wsn-device-tcp-server/src/main/java/de/uniluebeck/itm/tcp/server/utils/ServerDevice.java
+++ tr.wsn-device-tcp-server/src/main/java/de/uniluebeck/itm/tcp/server/utils/ServerDevice.java
@@ -20,8 +20,6 @@ import de.uniluebeck.itm.devicedriver.async.QueuedDeviceAsync;
 import de.uniluebeck.itm.devicedriver.async.thread.PausableExecutorOperationQueue;
 import de.uniluebeck.itm.metadaten.metadatenservice.metadatacollector.IMetaDataCollector;
 import de.uniluebeck.itm.metadaten.metadatenservice.metadatacollector.MetaDataCollector;
-import de.uniluebeck.itm.metadatenservice.MetaDatenService;
-import de.uniluebeck.itm.metadatenservice.iMetaDatenService;
 import de.uniluebeck.itm.tcp.jaxdevices.JaxbDevice;
 import de.uniluebeck.itm.tcp.jaxdevices.JaxbDeviceList;
 
@@ -123,7 +121,7 @@ public class ServerDevice {
 		
 		if(metaDaten){
 			try{
-				new MetadatenThread(new File(configPath), new File(sensorsPath), collectorList).start();
+				new MetaDataThread(new File(configPath), new File(sensorsPath), collectorList).start();
 			}catch(final NullPointerException es){
 				log.info("a config-file was not found, the Server will start without MetaDataService ");
 				log.error(es.getMessage(),es);
@@ -233,86 +231,5 @@ public class ServerDevice {
 	 */
 	public Map<String, DeviceAsync> getDeviceList() {
 		return deviceList;
-	}
-}
-
-/**
- * Thread for connecting to the MetadataService
- * @author Andreas Maier
- * @author Bjoern Schuett
- *
- */
-class MetadatenThread extends Thread {
-	
-	/**
-	 * the logger.
-	 */
-	private static Logger log = LoggerFactory.getLogger(MetadatenThread.class);
-	
-	/**
-	 * time to wait between the connections attemps
-	 */
-	private final static int WAITTIME = 600000;
-	
-	/**
-	 * 
-	 */
-	private boolean running = true;
-	
-	/**
-	 * The metaDatenService
-	 */
-	private iMetaDatenService mclient = null;
-	
-	/**
-	 * the Path of the config-file (config.xml)
-	 */
-	private File configPath = null;
-	
-	/**
-	 * the Path of the config-file (sensors.xml)
-	 */
-	private File sensorsPath = null;
-	/**
-	 * a List with the MetaDataCollectors for every existing Device
-	 */
-	private List<IMetaDataCollector> collectorList = null;
-	
-	/**
-	 * Constructor .
-	 * @param configPath the Path of config.xml
-	 * @param sensorsPath the Path of sensors.xml
-	 * @param collectorList a List with the MetaDataCollectors for every existing Device
-	 */
-	MetadatenThread(final File configPath, final File sensorsPath, final List<IMetaDataCollector> collectorList){
-		this.configPath = configPath;
-		this.sensorsPath = sensorsPath;
-		this.collectorList = collectorList;
-	}
-
-	public boolean isRunning() {
-		return running;
-	}
-	
-	@Override
-	public void run(){
-		while(running){
-			
-			try{
-				mclient = new MetaDatenService (configPath,sensorsPath);
-				for(IMetaDataCollector mcollector : collectorList){
-					mclient.addMetaDataCollector(mcollector);
-				}
-				running = false;
-			}catch(final Exception ex){
-				log.info("No Metadatenservice were found, the Server will try to connect in 10 Minutes again");
-				log.error("No Metadatenservice were found, the Server will try to connect in 10 Minutes again",ex);
-				try {
-					Thread.sleep(WAITTIME);
-				} catch (final InterruptedException e) {
-					log.error("",e);
-				}
-			}
-		}
 	}
 }
