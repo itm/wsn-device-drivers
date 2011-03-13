fd18ca864762781a9977a9d5f65a9becefcc773b
mlegenhausen <mlegenhausen@github.com>
2011-03-13 13:20:04 +0000

    POM clean ups. Jennic und PacemateConnection added for easier connection finding.
    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@748 0a1f239c-e987-4cba-9274-3ef986406e12

---
+++ rsc.drivers/rsc.drivers.jennic/src/main/java/de/uniluebeck/itm/devicedriver/jennic/JennicConnection.java
@@ -0,0 +1,13 @@
+package de.uniluebeck.itm.devicedriver.jennic;
+
+import de.uniluebeck.itm.devicedriver.generic.iSenseSerialPortConnection;
+
+
+/**
+ * Helper class for easier finding an apropriate connection for jennic.
+ * 
+ * @author Malte Legenhausen
+ */
+public class JennicConnection extends iSenseSerialPortConnection {
+
+}
---
+++ rsc.drivers/rsc.drivers.pacemate/src/main/java/de/uniluebeck/itm/devicedriver/pacemate/PacemateConnection.java
@@ -0,0 +1,13 @@
+package de.uniluebeck.itm.devicedriver.pacemate;
+
+import de.uniluebeck.itm.devicedriver.generic.iSenseSerialPortConnection;
+
+
+/**
+ * Helper class for easier finding an apropriate connection for pacemate.
+ * 
+ * @author Malte Legenhausen
+ */
+public class PacemateConnection extends iSenseSerialPortConnection {
+
+}
