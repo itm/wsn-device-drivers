b2219f10a746a9b3e6b6fafc8da17b15a6ff91ca
fkausche <fkausche@github.com>
2011-03-16 10:57:46 +0000

    git-svn-id: https://svn.itm.uni-luebeck.de/fallstudie2010/sources@774 0a1f239c-e987-4cba-9274-3ef986406e12

--- rsc.apps/rsc.apps.overlayclient/src/main/java/de/uniluebeck/itm/overlayclient/Main.java
+++ rsc.apps/rsc.apps.overlayclient/src/main/java/de/uniluebeck/itm/overlayclient/Main.java
@@ -11,30 +11,31 @@ import org.apache.commons.cli.*;
 import de.uniluebeck.itm.metadaten.remote.entity.Capability;
 
 /**
- * The Class Main.
- * Console program for the overlay client.
+ * The Class Main. Console program for the overlay client.
  */
 public class Main {
 
 	/** The version. */
 	private static double version = 1.0;
-	
-	/** The ip regex, to validate the server-address */
+
+	/** The ip regex, to validate the server-address. */
 	private static String ipRegex = "(((\\d{1,3}.){3})(\\d{1,3}))";
-	
-	/** The capabilityList regex, to validate the List of capabilities */
+
+	/** The capabilityList regex, to validate the List of capabilities. */
 	private static String capListRegex = "[[A-Za-z:]+,[0-9]*,[A-Za-z:]*]+(?:;[[A-Za-z:]+,[0-9]*,[A-Za-z:]*]+)*";
-	
+
 	/** The valid input gets false, when one of the input-parameters is wrong. */
 	private static boolean validInput = true;
 
 	/**
 	 * The main method.
-	 *
-	 * @param args the arguments
-	 * @throws IOException Signals that an I/O exception has occurred.
+	 * 
+	 * @param args
+	 *            the arguments
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
 	 */
-	public static void main(String[] args) throws IOException{
+	public static void main(final String[] args) throws IOException {
 		// create Options object
 		Option helpOption = new Option("help", "print this message");
 		Option versionOption = new Option("version",
@@ -49,22 +50,26 @@ public class Main {
 		options.addOption("id", true, "id to search for");
 		options.addOption("microcontroller", true,
 				"microcontroller to search for");
-		options.addOption("capabilities", true, "capabilities you want to search for in this format: name,default,datatype;name,default,datatype");
+		options.addOption(
+				"capabilities",
+				true,
+				"capabilities you want to search for in this format: name,default,datatype;name,default,datatype");
 		options.addOption("username", true, "username to connect to the server");
 		options.addOption("password", true, "password to connect to the server");
 		options.addOption("server", true, "IP-Adress of the server");
 		options.addOption("serverPort", true, "Port of the server");
 		options.addOption("clientPort", true, "Port of the client");
-		options.addOption("searchIP", true, "search for a device with the given IP-Address");
-		options.addOption("description", true, "search for a device with the given description");
+		options.addOption("searchIP", true,
+				"search for a device with the given IP-Address");
+		options.addOption("description", true,
+				"search for a device with the given description");
 
 		CommandLineParser parser = new GnuParser();
 		CommandLine cmd = null;
-		if(args.length == 0){
-			//if there is no input, print help message
+		if (args.length == 0) {
+			// if there is no input, print help message
 			printHelp(options);
-		}
-		else{
+		} else {
 			try {
 				cmd = parser.parse(options, args);
 			} catch (ParseException e) {
@@ -80,21 +85,21 @@ public class Main {
 					System.out.println(version);
 				}
 
-				//parameters for connection
+				// parameters for connection
 				String server = cmd.getOptionValue("server");
 				String serverPort = cmd.getOptionValue("serverPort");
 				String clientPort = cmd.getOptionValue("clientPort");
 				String username = cmd.getOptionValue("username");
 				String password = cmd.getOptionValue("password");
-				
-				//parameters for searching
+
+				// parameters for searching
 				String id = cmd.getOptionValue("id");
 				String microcontroller = cmd.getOptionValue("microcontroller");
 				String capabilities = cmd.getOptionValue("capabilities");
 				String searchIP = cmd.getOptionValue("searchIP");
 				String description = cmd.getOptionValue("description");
-				
-				//Begin: validate input-data
+
+				// Begin: validate input-data
 				if (server == null) {
 					System.out.println("Wrong input: Please enter server!");
 					validInput = false;
@@ -107,45 +112,55 @@ public class Main {
 					}
 				}
 				if (serverPort == null) {
-					System.out.println("Wrong input: Please enter port of the server!");
+					System.out
+							.println("Wrong input: Please enter port of the server!");
 					validInput = false;
-				}
-				else{
-					if(!serverPort.matches("\\d*")){
-						System.out.println("Wrong input: This is no valid server-port number.");
-				    	validInput = false;
+				} else {
+					if (!serverPort.matches("\\d*")) {
+						System.out
+								.println("Wrong input: This is no valid server-port number.");
+						validInput = false;
 					}
 				}
-				if(clientPort != null && !clientPort.matches("\\d*")){
-					System.out.println("Wrong input: This is no valid client-port number.");
-			    	validInput = false;
-				}
-				if (id == null && microcontroller == null && capabilities == null && searchIP == null && description == null) {
-					System.out.println("Wrong input: Please enter id, microcontroller, capabilities, searchIP or description to search for at least one of these parameters!");
+				if (clientPort != null && !clientPort.matches("\\d*")) {
+					System.out
+							.println("Wrong input: This is no valid client-port number.");
 					validInput = false;
 				}
-				if(searchIP != null){
-					if (!searchIP.matches(ipRegex) && !searchIP.equals("localhost")) {
+				if (id == null && microcontroller == null
+						&& capabilities == null && searchIP == null
+						&& description == null) {
+					System.out
+							.println("Wrong input: Please enter id, microcontroller, capabilities, searchIP or description to search for at least one of these parameters!");
+					validInput = false;
+				}
+				if (searchIP != null) {
+					if (!searchIP.matches(ipRegex)
+							&& !searchIP.equals("localhost")) {
 						System.out
 								.println("Wrong input: Please enter searchIP as IP-Address.");
 						validInput = false;
 					}
 				}
-				if(id != null && !id.matches("\\d*")){
-					System.out.println("Wrong input: Please enter id as integer!");
+				if (id != null && !id.matches("\\d*")) {
+					System.out
+							.println("Wrong input: Please enter id as integer!");
 					validInput = false;
 				}
-				if (capabilities != null){
-					if(!capabilities.matches(capListRegex)){
-						System.out.println("Wrong input: Please enter capability-names like name,default,datatype;name,default,datatype or name,default,datatype;name!");
+				if (capabilities != null) {
+					if (!capabilities.matches(capListRegex)) {
+						System.out
+								.println("Wrong input: Please enter capability-names like name,default,datatype;name,default,datatype or name,default,datatype;name!");
 						validInput = false;
 					}
 				}
-			    //End: validate input-data
-				
-				if(validInput){
-					//username and password is required to connect to the server
-					if (server != null && (username == null && password == null || username == null)) {
+				// End: validate input-data
+
+				if (validInput) {
+					// username and password is required to connect to the
+					// server
+					if (server != null
+							&& (username == null && password == null || username == null)) {
 						System.out.println("Username and Password is missing.");
 						BufferedReader in = new BufferedReader(
 								new InputStreamReader(System.in));
@@ -161,44 +176,50 @@ public class Main {
 						System.out.print("Password: ");
 						password = in.readLine();
 					}
-					
-					//in case of searching for capabilites,
-					//the paramteter 'capabilites' holds the capabilites in the format: name,default,datatype;name,default,datatype
-					//default and datatype are optional
+
+					// in case of searching for capabilites,
+					// the paramteter 'capabilites' holds the capabilites in the
+					// format: name,default,datatype;name,default,datatype
+					// default and datatype are optional
 					List<Capability> capabilityList = null;
-					if(capabilities != null){
+					if (capabilities != null) {
 						capabilityList = new ArrayList<Capability>();
 						String[] singleCapabilities = capabilities.split(";");
-						for(int i = 0; i < singleCapabilities.length; i++){
+						for (int i = 0; i < singleCapabilities.length; i++) {
 							String capability = singleCapabilities[i];
 							String[] capabilityAttr = capability.split(",");
 							Capability capabilityObject = new Capability();
-							for(int j = 0; j < capabilityAttr.length; j++){
-								if(j == 0){
-									String capabilityName = "urn:wisebed:node:capability:"+capabilityAttr[0];
+							for (int j = 0; j < capabilityAttr.length; j++) {
+								if (j == 0) {
+									String capabilityName = "urn:wisebed:node:capability:"
+											+ capabilityAttr[0];
 									capabilityObject.setName(capabilityName);
-								}else if(j == 1){
-									if(capabilityAttr[1].matches("\\d*")){
-										int capabilityDefault = Integer.parseInt(capabilityAttr[1]);
-										capabilityObject.setCapDefault(capabilityDefault);
-									}
-									else{
+								} else if (j == 1) {
+									if (capabilityAttr[1].matches("\\d*")) {
+										int capabilityDefault = Integer
+												.parseInt(capabilityAttr[1]);
+										capabilityObject
+												.setCapDefault(capabilityDefault);
+									} else {
 										String capabilityDatatype = capabilityAttr[1];
-										capabilityObject.setDatatype(capabilityDatatype);
+										capabilityObject
+												.setDatatype(capabilityDatatype);
 									}
-								}
-								else if(j == 2){
+								} else if (j == 2) {
 									String capabilityDatatype = capabilityAttr[2];
-									capabilityObject.setDatatype(capabilityDatatype);
+									capabilityObject
+											.setDatatype(capabilityDatatype);
 								}
 							}
 							capabilityList.add(capabilityObject);
 						}
 					}
-					
-					OverlayClient metaService = new OverlayClient(username, password, server, serverPort, clientPort);
 
-					metaService.searchDevice(id, microcontroller, capabilityList, description, searchIP);
+					OverlayClient metaService = new OverlayClient(username,
+							password, server, serverPort, clientPort);
+
+					metaService.searchDevice(id, microcontroller,
+							capabilityList, description, searchIP);
 				}
 			}
 		}
@@ -206,19 +227,27 @@ public class Main {
 
 	/**
 	 * Prints the help.
-	 *
-	 * @param options the options, which are registered.
+	 * 
+	 * @param options
+	 *            the options, which are registered.
 	 */
-	public static void printHelp(Options options){
+	public static void printHelp(final Options options) {
 		System.out.println("Examples:");
-		System.out.println("Search by id of the node: -id 123 -server 141.48.65.111 -serverPort 8080");
-		//TODO Example for microcontroller
-		System.out.println("Search by one capability of the node: -capabilities light -server 141.48.65.111 -serverPort 8080");
-		System.out.println("Search by three capabilities names: -capabilities light;temperature;gas -server 141.48.65.111 -serverPort 8080");
-		System.out.println("Search by three capabilities with name,default,datatype: -capabilities light,6,int;temperature,7,int;gas,3,int -server 141.48.65.111 -serverPort 8080");
-		System.out.println("Search by description of the node: -description wisebed -server 141.48.65.111 -serverPort 8080");
-		System.out.println("Search by IP-Address of the node: -searchIP 141.49.65.111 -server 141.48.65.111 -serverPort 8080");
-		System.out.println("Search by IP-Address and description of the node: -searchIP 141.49.65.111 -description wisebed -server 141.48.65.111 -serverPort 8080");
+		System.out
+				.println("Search by id of the node: -id 123 -server 141.48.65.111 -serverPort 8080");
+		// TODO Example for microcontroller
+		System.out
+				.println("Search by one capability of the node: -capabilities light -server 141.48.65.111 -serverPort 8080");
+		System.out
+				.println("Search by three capabilities names: -capabilities light;temperature;gas -server 141.48.65.111 -serverPort 8080");
+		System.out
+				.println("Search by three capabilities with name,default,datatype: -capabilities light,6,int;temperature,7,int;gas,3,int -server 141.48.65.111 -serverPort 8080");
+		System.out
+				.println("Search by description of the node: -description wisebed -server 141.48.65.111 -serverPort 8080");
+		System.out
+				.println("Search by IP-Address of the node: -searchIP 141.49.65.111 -server 141.48.65.111 -serverPort 8080");
+		System.out
+				.println("Search by IP-Address and description of the node: -searchIP 141.49.65.111 -description wisebed -server 141.48.65.111 -serverPort 8080");
 		System.out.println("");
 
 		// for help statement
--- rsc.apps/rsc.apps.overlayclient/src/main/java/de/uniluebeck/itm/overlayclient/OverlayClient.java
+++ rsc.apps/rsc.apps.overlayclient/src/main/java/de/uniluebeck/itm/overlayclient/OverlayClient.java
@@ -8,113 +8,123 @@ import de.uniluebeck.itm.metadaten.remote.entity.Capability;
 import de.uniluebeck.itm.metadaten.remote.entity.Node;
 
 /**
- * The Class OverlayClient.
- * Connects to the MetaDataServer and searches for nodes in the network.
+ * The Class OverlayClient. Connects to the MetaDataServer and searches for
+ * nodes in the network.
  */
 public class OverlayClient {
-	
+
 	private String username;
 	private String password;
 	private String server;
 	private String serverPort;
 	private String clientPort;
-	
+
 	/**
 	 * Instantiates a new overlay client.
-	 *
-	 * @param username the username
-	 * @param password the password
-	 * @param server the server
-	 * @param serverPort the server port
-	 * @param clientPort the client port
+	 * @param username
+	 *            the username
+	 * @param password
+	 *            the password
+	 * @param server
+	 *            the server
+	 * @param serverPort
+	 *            the server port
+	 * @param clientPort
+	 *            the client port
 	 */
-	public OverlayClient(String username, String password, String server, String serverPort, String clientPort) {
+	public OverlayClient(final String username, final String password, final String server,
+			final String serverPort, final String clientPort) {
 		this.username = username;
 		this.password = password;
 		this.server = server;
 		this.serverPort = serverPort;
 		this.clientPort = clientPort;
 	}
-	
+
 	/**
 	 * Search device with the given id, microcontroller and/or capabilities.
-	 *
-	 * @param id 
+	 * @param id
 	 * @param microcontroller
 	 * @param capabilities
-	 * @throws Exception the exception
+	 * @throws Exception
+	 *             the exception
 	 */
-	public void searchDevice(String id, String microcontroller, List<Capability> capabilities, String description, String searchIP){
-		//connecting to the server
+	public void searchDevice(final String id, final String microcontroller,
+			final List<Capability> capabilities, final String description, final String searchIP) {
+		// connecting to the server
 		MetaDatenClient client = null;
 		try {
-			if(clientPort != null){
-				client = new MetaDatenClient(username, password, server, Integer.valueOf(serverPort), Integer.valueOf(clientPort));
-	
-			}else{
-				client = new MetaDatenClient(username, password, server, Integer.valueOf(serverPort));
+			if (clientPort != null) {
+				client = new MetaDatenClient(username, password, server,
+						Integer.valueOf(serverPort),
+						Integer.valueOf(clientPort));
+
+			} else {
+				client = new MetaDatenClient(username, password, server,
+						Integer.valueOf(serverPort));
 			}
-		} catch(Exception e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Could not connect to server!");
 		}
-		
-		//searching by example
+
+		// searching by example
 		Node queryExample = new Node();
-		if(id != null){
-			//searching device with given id
-			queryExample.setId(id);		
+		if (id != null) {
+			// searching device with given id
+			queryExample.setId(id);
 		}
-		if(microcontroller != null){
-			//searching device with given microcontroller
-			queryExample.setMicrocontroller(microcontroller);	
-		}	
-		if(capabilities != null){
-			//searching device with given capabilites
+		if (microcontroller != null) {
+			// searching device with given microcontroller
+			queryExample.setMicrocontroller(microcontroller);
+		}
+		if (capabilities != null) {
+			// searching device with given capabilites
 			queryExample.setCapabilityList(capabilities);
 		}
-		if(description != null){
-			//searching device with given description
+		if (description != null) {
+			// searching device with given description
 			queryExample.setDescription(description);
 		}
-		if(searchIP != null){
-			//searching device with given IP-Address
+		if (searchIP != null) {
+			// searching device with given IP-Address
 			queryExample.setIpAddress(searchIP);
 		}
-		String query = ""; // String for searching by query. This is not used here.
+		String query = ""; // String for searching by query. This is not used
+							// here.
 		List<Node> results = new ArrayList<Node>();
 		try {
-			results = client.search(queryExample, query);  	//
+			results = client.search(queryExample, query); //
 		} catch (Exception e) {
 			System.out.println("Error while searching the node.");
 		}
-		
-		//printing the results
+
+		// printing the results
 		System.out.println("Results: " + results.size());
 		for (Node node : results) {
 			printNode(node);
 		}
 	}
-	
+
 	/**
 	 * Prints the node.
-	 *
-	 * @param node the node
+	 * @param node
+	 *            the node
 	 */
-	public void printNode(Node node){
-		System.out.println("ID: "+node.getId());
-		System.out.println("Description: "+node.getDescription());
-		System.out.println("Microcontroller: "+node.getMicrocontroller());
+	public void printNode(final Node node) {
+		System.out.println("ID: " + node.getId());
+		System.out.println("Description: " + node.getDescription());
+		System.out.println("Microcontroller: " + node.getMicrocontroller());
 		List<Capability> capabilities = node.getCapabilityList();
 		System.out.println("Capabilites:");
-		for(int i = 0; i < capabilities.size(); i++){
+		for (int i = 0; i < capabilities.size(); i++) {
 			System.out.println(capabilities.get(i).getId());
 			System.out.println(capabilities.get(i).getName());
 			System.out.println(capabilities.get(i).getUnit());
 			System.out.println(capabilities.get(i).getDatatype());
 			System.out.println(capabilities.get(i).getCapDefault());
 		}
-		System.out.println("Timestamp: "+node.getTimestamp());
-		System.out.println("IP-Address: "+node.getIpAddress());
+		System.out.println("Timestamp: " + node.getTimestamp());
+		System.out.println("IP-Address: " + node.getIpAddress());
 		System.out.println();
 	}
 }
