83db6134485eb044522ec154d34c26913934b268
mlegenhausen <mlegenhausen@gmail.com>
2011-03-22 17:46:56 +0100

    One jar plugin added. last commit error fixed.

--- rsc.apps/rsc.apps.messenger/src/test/java/de/uniluebeck/itm/rsc/apps/messenger/MessengerTest.java
+++ rsc.apps/rsc.apps.messenger/src/test/java/de/uniluebeck/itm/rsc/apps/messenger/MessengerTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package de.uniluebeck.itm.rsc.apps.messenger;
 
 import org.junit.Test;
@@ -25,31 +24,3 @@ public class MessengerTest {
 	
 	*/
 }
-=======
-package de.uniluebeck.itm.rsc.apps.messenger;
-
-import org.junit.Test;
-
-public class MessengerTest {
-
-	@Test
-	public void emptyTest() {
-		
-	}
-	
-	/**
-	public void testSend(){
-		Messenger messenger = new Messenger();
-		messenger.connect();
-	    messenger.send("68656c6c6f");
-	    try {
-	    	Thread.sleep(1000);
-	    } catch (Exception ex) {
-	    	System.out.println(ex);
-	    }
-	    assertEquals(true, messenger.isSent());
-	}
-	
-	*/
-}
->>>>>>> 758c6d9624644e0f291e5d9d7f39fcb3b538afcc
