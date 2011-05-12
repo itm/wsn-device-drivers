b735a195a8f32b74c0d1871abe11390b34ccd483
Daniel Bimschas <daniel@bimschas.com>
2011-05-12 10:49:12 +0200

    Closed #6, #7, #8 (added ConnectionFactory, DeviceFactory and DeviceAsyncFactory)

---
+++ factories/src/main/java/de/uniluebeck/itm/wsn/drivers/factories/ConnectionFactory.java
@@ -0,0 +1,28 @@
+package de.uniluebeck.itm.wsn.drivers.factories;
+
+import de.uniluebeck.itm.wsn.drivers.core.serialport.SerialPortConnection;
+import de.uniluebeck.itm.wsn.drivers.isense.iSenseSerialPortConnection;
+import de.uniluebeck.itm.wsn.drivers.pacemate.PacemateSerialPortConnection;
+import de.uniluebeck.itm.wsn.drivers.telosb.TelosbSerialPortConnection;
+
+public abstract class ConnectionFactory {
+
+	public static SerialPortConnection create(DeviceType deviceType) {
+		switch (deviceType) {
+		case ISENSE:
+			return new iSenseSerialPortConnection();
+		case PACEMATE:
+			return new PacemateSerialPortConnection();
+		case TELOSB:
+			return new TelosbSerialPortConnection();
+		}
+		throw new RuntimeException("Unhandled device type \"" + deviceType
+				+ "\". Maybe someone forgot to add this (new) device type to " + ConnectionFactory.class.getName()
+				+ "?");
+	}
+	
+	public static SerialPortConnection create(String deviceType) {
+		return create(DeviceType.fromString(deviceType));
+	}
+
+}
