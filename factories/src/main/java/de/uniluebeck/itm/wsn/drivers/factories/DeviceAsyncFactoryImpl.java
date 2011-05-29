0db34cdab69a6d7cd4a7acb7f914ff8b874bb4cd
Daniel Bimschas <daniel@bimschas.com>
2011-05-29 10:32:28 +0200

    Closed #22 (Make factory methods non-static and add Guice support)

--- factories/src/main/java/de/uniluebeck/itm/wsn/drivers/factories/DeviceAsyncFactoryImpl.java
+++ factories/src/main/java/de/uniluebeck/itm/wsn/drivers/factories/DeviceAsyncFactoryImpl.java
@@ -23,13 +23,35 @@
 
 package de.uniluebeck.itm.wsn.drivers.factories;
 
+import com.google.inject.Inject;
 import de.uniluebeck.itm.wsn.drivers.core.Connection;
+import de.uniluebeck.itm.wsn.drivers.core.Device;
 import de.uniluebeck.itm.wsn.drivers.core.async.DeviceAsync;
 import de.uniluebeck.itm.wsn.drivers.core.async.OperationQueue;
+import de.uniluebeck.itm.wsn.drivers.core.async.QueuedDeviceAsync;
 
-public interface DeviceAsyncFactory {
+public class DeviceAsyncFactoryImpl implements DeviceAsyncFactory {
 
-	DeviceAsync create(DeviceType deviceType, Connection connection, OperationQueue operationQueue);
+	private DeviceFactory deviceFactory;
 
-	DeviceAsync create(String deviceType, Connection connection, OperationQueue operationQueue);
+	@Inject
+	public DeviceAsyncFactoryImpl(final DeviceFactory deviceFactory) {
+		this.deviceFactory = deviceFactory;
+	}
+
+	@Override
+	public DeviceAsync create(final DeviceType deviceType, final Connection connection,
+							  final OperationQueue operationQueue) {
+
+		Device<? extends Connection> device = deviceFactory.create(deviceType, connection);
+		return new QueuedDeviceAsync(operationQueue, device);
+	}
+
+	@Override
+	public DeviceAsync create(final String deviceType, final Connection connection,
+							  final OperationQueue operationQueue) {
+
+		return create(DeviceType.fromString(deviceType), connection, operationQueue);
+	}
+
 }
