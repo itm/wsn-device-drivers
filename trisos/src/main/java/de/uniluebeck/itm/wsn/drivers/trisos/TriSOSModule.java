cdfba3abc0da29e753b57126e54a1548ebf4284d
Torsten Teubler <teubler@fh-luebeck.de>
2012-12-10 09:51:18 +0100

    Unused import removed.

--- trisos/src/main/java/de/uniluebeck/itm/wsn/drivers/trisos/TriSOSModule.java
+++ trisos/src/main/java/de/uniluebeck/itm/wsn/drivers/trisos/TriSOSModule.java
@@ -13,7 +13,6 @@ import de.uniluebeck.itm.wsn.drivers.core.operation.*;
 import de.uniluebeck.itm.wsn.drivers.core.serialport.SerialPortConnection;
 import de.uniluebeck.itm.wsn.drivers.core.serialport.SerialPortProgrammingMode;
 import de.uniluebeck.itm.wsn.drivers.core.serialport.SerialPortProgrammingModeInterceptor;
-import de.uniluebeck.itm.wsn.drivers.isense.iSenseResetOperation;
 
 import javax.annotation.Nullable;
 import java.util.Map;
